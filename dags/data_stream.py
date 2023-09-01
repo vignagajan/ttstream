b4e6f45c17f107babaadd1f88bb96c3b6451e3aa
vignagajan <my.soclntwrks@gmail.com>
2023-09-01 16:32:00 +0000

    fix(airflow): use yfinance library

--- dags/data_stream.py
+++ dags/data_stream.py
@@ -10,44 +10,20 @@ default_args = {
 
 # def stream_stocks(tickers):
 import requests
-import json
-import random
 import time
+import random
 
-import requests
-
-
-def get_yahoo_data(tickers):
+def get_yahoo_data(tickers,period="1d",interval="1m"):
     
-    cookie = None
+    import yfinance as yf
+    # create ticker for Apple Stock
+    ticker = yf.Ticker(tickers)
+    # get data of the most recent date
+    data = ticker.history(period=period,interval=interval)
 
-    user_agent_key = "User-Agent"
-    user_agent_value = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+    return data.to_json()
 
-    headers = {user_agent_key: user_agent_value}
-    response = requests.get(
-        "https://fc.yahoo.com", headers=headers, allow_redirects=True
-    )
-
-    if not response.cookies:
-        raise Exception("Failed to obtain Yahoo auth cookie.")
-
-    cookie = list(response.cookies)[0]
-
-    response = requests.get(
-        "https://query2.finance.yahoo.com/v8/finance/chart/"+tickers,
-        headers=headers,
-        cookies={cookie.name: cookie.value},
-        allow_redirects=True,
-    )
-    data = response.json()
-
-    if data is None:
-        raise Exception("Failed to retrieve Yahoo data.")
-
-    return data
-
-
+print(get_yahoo_data("AAPL"))
 
     
 
